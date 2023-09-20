0e612fe5ea718a227792540a169aaaad65f01efd
soo-willsoft <soo@logcompany.co.kr>
2023-09-20 18:45:08 +0900

    [INIT] 신규세팅

--- theme/coreda/intro.php
+++ theme/coreda/intro.php
@@ -1,75 +1,6 @@
 
 
-<style>
-body{overflow-y: hidden;}
-.container {
-	margin:0 auto !important;
-	padding:0;
-	width:100%;
-	display:block;
-	height:100vh;
-	background:#000 url('<?=G5_THEME_URL?>/img/launcher.png') no-repeat center;
-	background-size:cover;
-}
-
-/* .adm_title{background:#f9a62e;color:white;padding:5px 30px;font-size:1.2em; border-radius:25px;margin-bottom:20px;display:inline-block} */
-
-#btnDiv {
-  display: none;
-  text-align: center;
-  position:absolute;
-  bottom:15vh;
-  width:100%;
-  z-index:1000;
-}
-.btn_ly{width:85%;
-  text-align:center;
-  margin:0 auto;}
-
-#myProgress {
-  width: 100%;
-}
-
-#myBar {
-  width: 1%;
-  height: 2px;
-}
-
-.animate-bottom {
-  position: relative;
-  -webkit-animation-name: animatebottom;
-  -webkit-animation-duration: 1s;
-  animation-name: animatebottom;
-  animation-duration: 1s
-}
-
-.intro_title {position:fixed;bottom:10px;text-align:center;width:100%;}
-.intro_title .company {color: rgba(255, 255, 255, 0.75);}
-.intro_title .copyright {line-height:26px;}
-
-@-webkit-keyframes animatebottom {
-  from { bottom:-10%; opacity:0 }
-  to { bottom:15vh; opacity:1 }
-}
-
-@keyframes animatebottom {
-  from{ bottom:-10%; opacity:0 }
-  to{ bottom:15vh; opacity:1 }
-}
-
-@media screen and (max-width: 766px) {
-	.container {max-width: 100%;}
-}
-
-@media (min-width: 767px) {
-	body{background:#0b0c13}
-	.container{width:550px;margin:0 auto;}
-	#btnDiv{width:550px;}
-	.intro_title{width:550px;}
-}
-</style>
-
-
+<link rel="stylesheet" href="<?=G5_THEME_URL?>/css/scss/page/intro.css">
 
 <script >
 	var myVar;
@@ -133,10 +64,10 @@ body{overflow-y: hidden;}
 
 						}
 
-						if(res.result == "FAIL"){
+						/* if(res.result == "FAIL"){
 							alert("EHTEREUM ADDRESS is not registered. Please Sign In or Sign Up.");
 							window.location.href = "/bbs/login_pw.php";
-						}
+						} */
 
 						if(res.result == "ERROR"){
 							alert("ERROR");
@@ -161,7 +92,11 @@ body{overflow-y: hidden;}
 		<div id="myBar"></div>
 		<div id="btnDiv" class="animate-bottom">
 			<div class='btn_ly'>
-				<?include_once(G5_THEME_PATH.'/_include/lang.php')?>
+				<?
+				if(Multi_languge_USE){
+					include_once(G5_THEME_PATH.'/_include/lang.php');
+				}
+				?>
 					<a href="/bbs/login_pw.php" class="btn btn_wd btn_secondary login_btn">LOG IN</a>
 					<!-- <a href="javascript:auto_login()" class="btn btn_wd btn_primary login_btn">LOG IN</a> -->
 						<a href="/bbs/register_form.php" class="btn btn_wd btn_primary signup_btn">SIGN UP</a>
